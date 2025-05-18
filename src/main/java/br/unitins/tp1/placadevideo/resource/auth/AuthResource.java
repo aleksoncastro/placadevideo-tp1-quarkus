1a5f05ff44f3567f84670a7b735fe25da5f5a077
Hallan Christian <hallanchristiancruz@gmail.com>
2025-05-18 18:13:07 -0300

    Refactor login methods in AuthResource to improve role validation and response handling for clients and admins

--- src/main/java/br/unitins/tp1/placadevideo/resource/auth/AuthResource.java
+++ src/main/java/br/unitins/tp1/placadevideo/resource/auth/AuthResource.java
@@ -4,6 +4,7 @@ import org.jboss.logging.Logger;
 
 import br.unitins.tp1.placadevideo.dto.request.AuthRequestDTO;
 import br.unitins.tp1.placadevideo.dto.response.UsuarioResponseDTO;
+import br.unitins.tp1.placadevideo.model.usuario.Perfil;
 import br.unitins.tp1.placadevideo.model.usuario.Usuario;
 import br.unitins.tp1.placadevideo.resource.cliente.ClienteResource;
 import br.unitins.tp1.placadevideo.service.hash.HashService;
@@ -34,20 +35,47 @@ public class AuthResource {
 
     private static final Logger LOG = Logger.getLogger(ClienteResource.class);
 
+    // Login para clientes
     @POST
-    public Response login(AuthRequestDTO authDTO) {
-        LOG.info("Executando autorizacao do usuario");
+    @Path("/cliente")
+    public Response loginCliente(AuthRequestDTO authDTO) {
+        LOG.info("Executando login de cliente");
         String hash = hashService.getHashSenha(authDTO.senha());
 
         Usuario usuario = usuarioService.findByUsernameAndSenha(authDTO.username(), hash);
         if (usuario == null) {
-            return Response.status(Status.NOT_FOUND)
-                    .entity("Usuario não encontrado").build();
+            return Response.status(Status.NOT_FOUND).entity("Usuario não encontrado").build();
         }
-        return Response.ok(usuario)
+
+        // Verifica se tem a role USER (cliente)
+        if (usuario.getPerfil() != Perfil.USER) {
+            return Response.status(Status.UNAUTHORIZED).entity("Acesso restrito a clientes").build();
+        }
+
+        return Response.ok(UsuarioResponseDTO.valueOf(usuario))
                 .header("Authorization", jwtService.generateJwt(UsuarioResponseDTO.valueOf(usuario)))
                 .build();
-
     }
 
+    // Login para admins e funcionários
+    @POST
+    @Path("/admin")
+    public Response loginAdmin(AuthRequestDTO authDTO) {
+        LOG.info("Executando login de admin");
+        String hash = hashService.getHashSenha(authDTO.senha());
+
+        Usuario usuario = usuarioService.findByUsernameAndSenha(authDTO.username(), hash);
+        if (usuario == null) {
+            return Response.status(Status.NOT_FOUND).entity("Usuario não encontrado").build();
+        }
+
+        // Verifica se tem a role ADM
+        if (usuario.getPerfil() != Perfil.ADM) {
+            return Response.status(Status.UNAUTHORIZED).entity("Acesso restrito a administradores").build();
+        }
+
+        return Response.ok(UsuarioResponseDTO.valueOf(usuario))
+                .header("Authorization", jwtService.generateJwt(UsuarioResponseDTO.valueOf(usuario)))
+                .build();
+    }
 }
